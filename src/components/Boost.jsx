9c885a8b05609239dffd191f6b31a78d41d8db86
Nweke Chidi <nwekechidigodwin460@gmail.com>
2023-01-01 17:12:36 +0100

    boost section added
    Signed-off-by: Nweke Chidi <nwekechidigodwin460@gmail.com>

--- src/components/Boost.jsx
+++ src/components/Boost.jsx
@@ -1,7 +1,13 @@
+import ShortenDesktop from "../assets/Icons/bg-shorten-desktop.svg";
+
 function Boost() {
     return ( 
-        <section>
-            <h1>Boost</h1>
+        <section   className=" py-5 bg-LightPurple flex flex-col justify-center items-center"
+        style={{ backgroundImage: `url(${ShortenDesktop})` }}>
+            <h1 className="text-lg font-bold md:text-2xl">Boost your links today</h1>
+            <button className="my-3 px-5 py-2 border rounded-full bg-Cyan hover:bg-cyan-500 text-white hover:text-cyan-100">
+            Get Started
+          </button>
         </section>
      );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,6 +8,7 @@ import { FaInstagramSquare } from "react-icons/fa";
 
 function Footer() {
   return (
+    <div className="bg-DarkViolet" >
     <section className="px-6 py-8 sm:py-8 sm:px-10 2xl:max-w-7xl mx-auto 2xl:px-0 bg-DarkViolet flex flex-col justify-center items-center gap-8 sm:flex-row sm:items-start sm:justify-between">
       
       {/* Logo */}
@@ -20,26 +21,26 @@ function Footer() {
         <div>
           <h2 className="mb-3 font-bold">Features</h2>
           <ul>
-            <li className="hover:text-cyan-600 pb-1">Link Shortening</li>
-            <li className="hover:text-cyan-600 pb-1">Branded Links</li>
-            <li className="hover:text-cyan-600 pb-1">Analytics</li>
+            <li className="hover:text-cyan-600 pb-1 cursor-pointer">Link Shortening</li>
+            <li className="hover:text-cyan-600 pb-1 cursor-pointer">Branded Links</li>
+            <li className="hover:text-cyan-600 pb-1 cursor-pointer">Analytics</li>
           </ul>
         </div>
         <div >
           <h2 className="mb-3 font-bold">Reasources</h2>
           <ul>
-            <li className="hover:text-cyan-600 pb-1">Blog</li>
-            <li className="hover:text-cyan-600 pb-1">Developers</li>
-            <li className="hover:text-cyan-600 pb-1">Support</li>
+            <li className="hover:text-cyan-600 pb-1 cursor-pointer">Blog</li>
+            <li className="hover:text-cyan-600 pb-1 cursor-pointer">Developers</li>
+            <li className="hover:text-cyan-600 pb-1 cursor-pointer">Support</li>
           </ul>
         </div>
         <div>
           <h2 className="mb-3 font-bold">Company</h2>
           <ul>
-            <li className="hover:text-cyan-600 pb-1">About</li>
-            <li className="hover:text-cyan-600 pb-1">Our Team</li>
-            <li className="hover:text-cyan-600 pb-1">Careers</li>
-            <li className="hover:text-cyan-600 pb-1">Contact</li>
+            <li className="hover:text-cyan-600 pb-1 cursor-pointer">About</li>
+            <li className="hover:text-cyan-600 pb-1 cursor-pointer">Our Team</li>
+            <li className="hover:text-cyan-600 pb-1 cursor-pointer">Careers</li>
+            <li className="hover:text-cyan-600 pb-1 cursor-pointer">Contact</li>
           </ul>
         </div>
       </div>
@@ -76,6 +77,7 @@ function Footer() {
         </div>
       </div>
     </section>
+    </div>
   );
 }
 
